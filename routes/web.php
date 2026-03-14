beb9685075df26ea6db6ebd6b404e0974d289719
Victor Cobuci <cobuci80@gmail.com>
2026-03-14 17:52:31 -0300

    Add dashboard route with authentication middleware

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Livewire\Dashboard\Index;
 use App\Livewire\Login;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', Login::class)->name('login');
+Route::get('/dashboard', Index::class)->name('dashboard')->middleware('auth');
