a04776703f775fb0ee3bc7329f44ca42ebce58e9
Victor Cobuci <cobuci80@gmail.com>
2026-03-14 14:21:24 -0300

    Add Livewire dashboard index component and corresponding Blade view

---
+++ app/Livewire/Dashboard/Index.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Livewire\Dashboard;
+
+use Illuminate\View\View;
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render(): View
+    {
+        return view('livewire.dashboard.index');
+    }
+}
