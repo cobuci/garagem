059a5012139c6140186d6dca73286c6c95baeeb3
Victor Cobuci <cobuci80@gmail.com>
2026-03-13 22:47:25 -0300

    Refactor configuration files for consistent formatting and readability

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,8 @@ use Illuminate\Support\Str;
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
