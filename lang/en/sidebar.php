dc5603701627d63d15e54d8745f1490df9730b08
Victor Cobuci <cobuci80@gmail.com>
2026-03-14 20:05:56 -0300

    sidebar translations

--- lang/en/sidebar.php
+++ lang/en/sidebar.php
@@ -1,13 +1,15 @@
 <?php
 
 return [
-    'general'   => 'General',
-    'sales'     => 'Sales',
-    'dashboard' => 'Dashboard',
-    'orders'    => 'Orders',
-    'customers' => 'Customers',
-    'reports'   => 'Reports',
-    'settings'  => 'Settings',
-    'logout'    => 'Logout',
-    'user'      => 'User',
+    'general'    => 'General',
+    'sales'      => 'Sales',
+    'dashboard'  => 'Dashboard',
+    'orders'     => 'Orders',
+    'customers'  => 'Customers',
+    'reports'    => 'Reports',
+    'settings'   => 'Settings',
+    'logout'     => 'Logout',
+    'user'       => 'User',
+    'dark_mode'  => 'Dark Mode',
+    'light_mode' => 'Light Mode',
 ];
--- lang/pt_BR/sidebar.php
+++ lang/pt_BR/sidebar.php
@@ -1,13 +1,15 @@
 <?php
 
 return [
-    'general'   => 'Geral',
-    'sales'     => 'Vendas',
-    'dashboard' => 'Dashboard',
-    'orders'    => 'Pedidos',
-    'customers' => 'Clientes',
-    'reports'   => 'Relatórios',
-    'settings'  => 'Configurações',
-    'logout'    => 'Sair',
-    'user'      => 'Usuário',
+    'general'    => 'Geral',
+    'sales'      => 'Vendas',
+    'dashboard'  => 'Dashboard',
+    'orders'     => 'Pedidos',
+    'customers'  => 'Clientes',
+    'reports'    => 'Relatórios',
+    'settings'   => 'Configurações',
+    'logout'     => 'Sair',
+    'user'       => 'Usuário',
+    'dark_mode'  => 'Modo Escuro',
+    'light_mode' => 'Modo Claro',
 ];
