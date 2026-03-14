772a305f529076786d64cbfc63a387063029914f
Victor Cobuci <cobuci80@gmail.com>
2026-03-14 19:17:40 -0300

    Customer seeders

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Enums\Gender;
+use Database\Factories\CustomerFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -15,7 +16,6 @@ use Illuminate\Support\Carbon;
  * @property ?string $phone
  * @property ?string $email
  * @property ?string $zip_code
- * @property ?string $address
  * @property ?string $street
  * @property ?string $neighborhood
  * @property ?Carbon $created_at
@@ -24,8 +24,11 @@ use Illuminate\Support\Carbon;
  */
 class Customer extends Model
 {
+    /* @use HasFactory<CustomerFactory> */
     use HasFactory;
     use SoftDeletes;
+
+    protected $guarded = ['id'];
 
     protected $casts = [
         'gender' => Gender::class,
