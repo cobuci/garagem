29f480a927745873462094da9f8d5d5be7fd3aae
Victor Cobuci <cobuci80@gmail.com>
2026-03-14 19:15:47 -0300

    customer translations

---
+++ lang/en/customers.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'title'        => 'Customers',
+    'subtitle'     => 'Manage the list of registered customers.',
+    'new_customer' => 'New Customer',
+    'name'         => 'Name',
+    'phone'        => 'Phone',
+    'street'       => 'Street',
+    'empty'        => 'No customers found.',
+];
---
+++ lang/pt_BR/customers.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'title'        => 'Clientes',
+    'subtitle'     => 'Gerencie a lista de clientes cadastrados.',
+    'new_customer' => 'Novo Cliente',
+    'name'         => 'Nome',
+    'phone'        => 'Telefone',
+    'street'       => 'Rua',
+    'empty'        => 'Nenhum cliente encontrado.',
+];
