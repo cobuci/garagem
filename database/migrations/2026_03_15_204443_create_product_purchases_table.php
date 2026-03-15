b9c3f45d7d8cb4d1576262b49f37e82c264897cf
Victor Cobuci <cobuci80@gmail.com>
2026-03-15 17:56:33 -0300

    create product purchase table

---
+++ database/migrations/2026_03_15_204443_create_product_purchases_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_purchases', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained()->cascadeOnDelete();
+            $table->bigInteger('unit_cost');
+            $table->bigInteger('total_cost');
+            $table->integer('quantity');
+            $table->date('invoice_date')->nullable();
+            $table->date('payment_date')->nullable();
+            $table->date('due_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_purchases');
+    }
+};
