cce63bdeac486627a358608b9ae3af6133b80828
Victor Cobuci <cobuci80@gmail.com>
2026-03-15 14:57:20 -0300

    seeder

--- database/seeders/CategoryProductSeeder.php
+++ database/seeders/CategoryProductSeeder.php
@@ -10,8 +10,13 @@ class CategoryProductSeeder extends Seeder
 {
     public function run(): void
     {
+        $icons = ['tag', 'shopping-cart', 'beaker', 'home', 'truck', 'gift', 'puzzle-piece', 'identification', 'briefcase', 'archive-box'];
+
         Category::factory(10)
-            ->sequence(fn ($sequence) => ['sort_order' => $sequence->index])
+            ->sequence(fn ($sequence) => [
+                'sort_order' => $sequence->index,
+                'icon'       => $icons[$sequence->index % count($icons)],
+            ])
             ->has(Product::factory(20))
             ->create();
     }
