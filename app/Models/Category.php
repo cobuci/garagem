b2c0bb8f956387ca3098113f2cf9286c36b699fc
Victor Cobuci <cobuci80@gmail.com>
2026-03-15 14:14:46 -0300

    fix categories model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,15 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Carbon;
 
 /**
- * @property int     $id
- * @property string  $name
- * @property ?string $icon
+ * @property int       $id
+ * @property string    $name
+ * @property ?string   $icon
+ * @property ?Carbon   $created_at
+ * @property ?Carbon   $updated_at
+ * @property ?Carbon   $deleted_at
+ * @property Product[] $products
  */
 class Category extends Model
 {
     use HasFactory;
     use SoftDeletes;
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
 }
