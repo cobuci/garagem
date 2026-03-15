9d07130fced5f4be34c719814f11947a0eaa814c
Victor Cobuci <cobuci80@gmail.com>
2026-03-15 14:25:13 -0300

    insert products in seeder

---
+++ database/seeders/CategoryProductSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Database\Seeder;
+
+class CategoryProductSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Category::factory(10)
+            ->has(Product::factory(10))
+            ->create();
+    }
+}
