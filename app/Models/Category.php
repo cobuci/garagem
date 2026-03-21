372ee2ae85d31d65da16319e03157c663fac07c6
Victor Cobuci <cobuci80@gmail.com>
2026-03-21 15:51:37 -0300

    Update Category.php

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -25,6 +25,8 @@ class Category extends Model
     use HasFactory;
     use SoftDeletes;
 
+    protected $guarded = ['id'];
+
     public function products(): HasMany
     {
         return $this->hasMany(Product::class);
