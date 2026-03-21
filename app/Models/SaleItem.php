d5aae77d19e8b93ca1a698cba5f87662ef021ec2
Victor Cobuci <cobuci80@gmail.com>
2026-03-20 21:43:14 -0300

    Add SaleItemFactory and integrate factory support in SaleItem model

--- app/Models/SaleItem.php
+++ app/Models/SaleItem.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use App\Casts\MoneyCast;
+use Database\Factories\SaleItemFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Carbon;
@@ -22,6 +24,9 @@ use Illuminate\Support\Carbon;
  */
 class SaleItem extends Model
 {
+    /** @use HasFactory<SaleItemFactory> */
+    use HasFactory;
+
     protected $guarded = ['id'];
 
     protected function casts(): array
