52ad2b9d78db42d7c9db909482d8e4ffaf67c12e
Victor Cobuci <cobuci80@gmail.com>
2026-04-04 17:29:21 -0300

    Update LogoutController.php

--- app/Http/Controllers/Api/V1/Auth/LogoutController.php
+++ app/Http/Controllers/Api/V1/Auth/LogoutController.php
@@ -8,9 +8,6 @@ use Illuminate\Http\Request;
 
 class LogoutController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     */
     public function __invoke(Request $request): JsonResponse
     {
         $request->user()->currentAccessToken()->delete();
